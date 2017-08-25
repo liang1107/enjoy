647ec7ddf7f3ca709055574170a54bddf6f26138
Your Name <1158947323@qq.com>
2017-08-25 18:49:46 +0800

    enjoy

---
+++ md/more.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex" id="content1">
+        <header class="header">更多</header>
+        <div id="content">内容
+             <!-- <img  v-lazy.content1="url">  -->
+        </div>
+        
+        
+    </div>
+</template>
+
+
+<script>
+import Vue from "vue"
+// import { Lazyload } from 'mint-ui';
+// Vue.use(Lazyload);
+// Vue.component(Popup.name, Popup);
+    export default {
+        data(){
+            return {
+               url:"http://fuss10.elemecdn.com/7/85/e478e4b26af74f4539c79f31fde80jpeg.jpeg"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    image[lazy=loading] {
+        width: 40px;
+        height: 300px;
+        margin: auto;
+        }
+</style>
---
+++ md/user.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="flex">
+        <div id="userid">
+            <div class="userhead">
+                <div class="username">
+                    <img src="https://image.ricebook.com/avatar/1?imageView2/1/w/140/h/140"><br/>
+                        {{name}}
+                </div>
+            </div>
+            <div class="xian"></div>
+            <div class="quan">
+                <div class="left">Code 消费码</div>
+                <div class="right">></div>
+            </div>
+            <div class="xian"></div>
+            <div class="quan">
+                <div class="left">我的礼券</div>
+                <div class="right">></div>
+            </div>
+            <div class="xian"></div>
+            <div class="quan">
+                <div class="left">地址管理</div>
+                <div class="right">></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import "./../scss/user.scss"
+
+    export default {
+        data(){
+            return {
+                 name:""
+            }
+        },
+        mounted(){
+          this.name=localStorage.getItem("user")
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
